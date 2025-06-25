0afe3a656a931aa6a5e08bcf11c6531f4a2e815e
shiva <shiva@DESKTOP-33DC4ED>
2025-06-25 12:59:47 +0530

    Project with Contacts and Organiation

---
+++ ActualProject/src/test/java/createContactsPage/ToCreateContactsPage.java
@@ -0,0 +1,10 @@
+package createContactsPage;
+
+public class ToCreateContactsPage {
+
+	public static void main(String[] args) {
+		System.out.println("created contacts page");
+
+	}
+
+}
---
+++ ActualProject/src/test/java/createOrgPage/ToCreateOrgPage.java
@@ -0,0 +1,9 @@
+package createOrgPage;
+
+public class ToCreateOrgPage {
+
+	public static void main(String[] args) {
+		System.out.println("Created organaization page");
+	}
+
+}
